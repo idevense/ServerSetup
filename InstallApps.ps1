391b8362258411229a52c07cb4265abec71aec70
Evensen Idar <Idar.Evensen@hnikt.no>
2021-01-12 12:59:34 +0100

    added installapps script. Invoke-Expression "InstallApps.ps1 ""$chocoAppList"" ""$dismAppList"""

---
+++ InstallApps.ps1
@@ -0,0 +1,54 @@
+<#
+	Script that accepts 2 parameters like this: 
+	$chocoAppList = "7zip, googlechrome, .."
+	$dismAppList = "File-Services, FS-FileServer, .."
+	
+	you invoke the script like this:
+	Invoke-Expression "InstallApps.ps1 ""$chocoAppList"" ""$dismAppList"""
+	
+	Idar Evensen 2021
+#>
+Param(
+	[string]$chocoAppList,
+	[string]$dismAppList
+	)
+
+if ([string]::IsNullOrWhiteSpace($chocoAppList) -eq $false -or [string]::IsNullOrWhiteSpace($dismAppList) -eq $false)
+	try{
+		choco config get cacheLocation
+	}catch{
+		Write-Output "Chocolatey not detected, trying to install it now. Hope you activated the proxy..."
+		If ((Test-NetConnection -ComputerName www-proxy.helsenord.no -Port 8080).TcpTestSucceeded) {
+			Write-Output "proxy reachable, continuing"
+			$env:chocolateyProxyLocation = '172.29.3.232:8080'
+			Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	
+			choco source add -n=Artifactory -s="http://release.helsenord.no:8081/artifactory/api/nuget/Chocolatey-ServerSetups"
+			choco source remove -n=chocolatey
+		}
+		else { Write-Output "Could not reach proxy, is the server in the correct zone?" }
+	}
+	
+<# if $chocoApplist contains data, we try to choco install #>
+([string]::IsNullOrWhiteSpace($chocoAppList) -eq $false){
+Write-Host "Choco Apps will be installed"
+
+$appsToInstall = $chocoAppList -split "," | foreach { "$($.Trim())" }
+
+foreach ($app in $appsToInstall)
+	{
+		Write-Host "Installing $app"
+		& choco install $app /y | Write-Output
+	}
+	
+<# if $dismAppList contains data, we try to install windows features #>
+([string]::IsNullOrWhiteSpace($dismAppList) -eq $false){
+Write-Host "DISM features will be installed"
+
+$appsToInstall = $dismAppList -split "," | foreach { "$($.Trim())" }
+
+foreach ($app in $dismAppList)
+	{
+		Write-Host "Installing $app"
+		& choco install $app /y /source windowsfeatures | Write-Output
+	}
